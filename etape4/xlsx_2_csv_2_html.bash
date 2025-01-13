ae88caf6f9601f79da7b44ce9634bc15833a9640
Unlucky <ameliejulien31@free.fr>
2025-01-13 16:31:25 +0100

    Update xlsx_2_csv_2_html.bash

--- etape4/xlsx_2_csv_2_html.bash
+++ etape4/xlsx_2_csv_2_html.bash
@@ -3,13 +3,15 @@
 # Exécute un conteneur Docker en arrière-plan
 container_id=$(docker run --rm -dti sae103-excel2csv)
 
-# Vérifiez si le conteneur a démarré correctement
-if [ -z "$container_id" ]; then
-    echo "Erreur: Impossible de démarrer le conteneur Docker."
-    exit 1
-fi
-
 shopt -s nullglob  # Ignore les globs sans correspondance
+
+# Lire le fichier de correspondance des pays et codes ISO alpha-2
+declare -A country_iso_map
+for file in ./flags/small/*.png; do
+    filename=$(basename "$file" .png)
+    country_iso_map["$filename"]="$filename"
+done
+
 # Conversion des fichiers XLSX en CSV
 for file in *.xlsx; do
     docker cp "$file" "${container_id}:/tmp/${file}"
@@ -17,18 +19,64 @@ for file in *.xlsx; do
     docker cp "${container_id}:/tmp/${file%.xlsx}.csv" "${file%.xlsx}.csv"
 done
 
-# Vérifiez si le conteneur est toujours en cours d'exécution avant de l'arrêter
-if docker ps -q --filter "id=${container_id}" | grep -q .; then
-    docker stop "${container_id}"
-else
-    echo "Erreur: Le conteneur Docker n'est plus en cours d'exécution."
-fi
+# Arrêter le conteneur Docker
+docker stop "${container_id}"
+
+# Préparer une chaîne pour awk
+country_iso_map_string=""
+for key in "${!country_iso_map[@]}"; do
+    country_iso_map_string+="$key=${country_iso_map[$key]} "
+done
 
 # Conversion des fichiers CSV en HTML
 for csv_file in *.csv; do
     html_file="${csv_file%.csv}.html"
-    awk -F',' 'BEGIN {print "<table>"} 
-        NR==1 {print "<tr>"; for(i=1; i<=NF; i++) print "<th>"$i"</th>"; print "</tr>"} 
-        NR>1 {print "<tr>"; for(i=1; i<=NF; i++) print "<td>"$i"</td>"; print "</tr>"} 
-        END {print "</table>"}' "$csv_file" > "$html_file"
+    
+    # Générer le tableau HTML
+    awk -F',' -v map="$country_iso_map_string" '
+        BEGIN {
+            split(map, arr, " ")
+            for (i in arr) {
+                split(arr[i], kv, "=")
+                country_iso_map[kv[1]] = kv[2]
+            }
+
+            print "<!DOCTYPE html>"
+            print "<html>"
+            print "<head>"
+            print "<meta charset=\"utf-8\">"
+            print "</head>"
+            print "<body>"
+            print "<table border=\"1\">"
+        }
+        {
+            print "<tr>"
+            for(i=1; i<=NF; i++) {
+                print "<td>" $i "</td>"
+            }
+            print "</tr>"
+        }
+        END {print "</table></body></html>"}' "$csv_file" > "$html_file"
+    
+    # Ajouter les images après avoir créé le tableau
+    awk -F',' -v map="$country_iso_map_string" '
+        BEGIN {
+            split(map, arr, " ")
+            for (i in arr) {
+                split(arr[i], kv, "=")
+                country_iso_map[kv[1]] = kv[2]
+            }
+        }
+        {
+            print "<tr>"
+            for(i=1; i<=NF; i++) {
+                if (country_iso_map[$i] != "") {
+                    img_path = "./flags/small/" country_iso_map[$i] ".png"
+                    print "<td><img src=\"" img_path "\" alt=\"" $i "\" width=\"30\" height=\"30\"></td>"
+                } else {
+                    print "<td></td>"
+                }
+            }
+            print "</tr>"
+        }' "$csv_file" >> "$html_file"
 done
